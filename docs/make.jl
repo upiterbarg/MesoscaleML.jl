9bdb1d3222511da114c2c0b690d3fb207814e5a1
Ulyana Piterbarg <ulyana.piterbarg@gmail.com>
2020-09-28 18:55:09 -0400

    deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -11,5 +11,5 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "https://upiterbarg.github.io/MesoscaleML.jl"
+    repo = "https://github.com/upiterbarg/MesoscaleML.jl"
 )
